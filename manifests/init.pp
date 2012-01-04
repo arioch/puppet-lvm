dc214521b10f1d39a4062bd5c42c7d62acbc1c30
Tom De Vylder <tom@penumbra.be>
2012-01-04 15:00:35 +0100

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,72 +12,32 @@
 #
 # Sample Usage:
 #
-#	include lvm
-#	lvm::lv {
-#		"device1":
-#			ensure => present,
-#			size   => "10G",
-#			vg     => "vgStorage";
-#		"device2":
-#			ensure => absent,
-#			vg     => "vgStorage";
-#		"device3":
-#			ensure => extend,
-#			size   => "10G",
-#			vg     => "vgStorage";
-#		"device4":
-#			ensure => reduce,
-#			size   => "10G",
-#			vg     => "vgStorage";
-#	}
+# include lvm
+# lvm::lv {
+#   "device1":
+#     ensure => present,
+#     size   => "10G",
+#     vg     => "vgStorage";
+#   "device2":
+#     ensure => absent,
+#     vg     => "vgStorage";
+#   "device3":
+#     ensure => extend,
+#     size   => "10G",
+#     vg     => "vgStorage";
+#   "device4":
+#     ensure => reduce,
+#     size   => "10G",
+#     vg     => "vgStorage";
+# }
 #
-class lvm {
-	# Define: lv
-	# Parameters:
-	# ensure, vg, size=NONE
-	#
-	define lv (ensure, vg, size=NONE) {
-		case $ensure {
-			present: {
-				exec { "Create LVM device /dev/${vg}/${name}":
-					command => "lvcreate -L ${size} --name ${name} ${vg}",
-					unless  => "test -e /dev/${vg}/${name}",
-				}
-			}
+class lvm () {
+  include motd
+  motd::register{'lvm': }
 
-			extend: {
-				exec { "Extend LVM device /dev/${vg}/${name}":
-					command => "lvextend -L +${size} /dev/${vg}/${name}",
-					onlyif  => "test -e /dev/${vg}/${name}",
-				}
-			}
+  class { 'lvm::params': }
+  class { 'lvm::install': }
 
-			reduce: {
-				exec { "Reduce LVM device /dev/${vg}/${name}":
-					command => "lvreduce --force -L -${size} /dev/${vg}/${name}",
-					onlyif  => "test -e /dev/${vg}/${name}",
-				}
-			}
-
-			absent: {
-				exec { "Remove LVM device /dev/${vg}/${name}":
-					command => "lvremove --force /dev/${vg}/${name}",
-					onlyif  => "test -e /dev/${vg}/${name}",
-				}
-			}
-		}
-	}
-
-	# Class: install
-	#
-	#
-	class install {
-		package {
-			"lvm":
-				name   => "$lvm::params::lvm_name",
-				ensure => present;
-		}
-	}
-
-	include install
+  Class['lvm::params'] ->
+  Class['lvm::install']
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,17 +2,17 @@
 #
 #
 class lvm::params {
-	case $operatingsystem {
-		'ubuntu', 'debian': {
-			$lvm_name = "lvm2"
-		}
+  case $operatingsystem {
+    'ubuntu', 'debian': {
+      $lvm_name = "lvm2"
+    }
 
-		'centos', 'redhat', 'fedora', 'scientific': {
-			$lvm_name = "lvm2"
-		}
+    'centos', 'redhat', 'fedora', 'scientific': {
+      $lvm_name = "lvm2"
+    }
 
-		default: {
-			fail "Operating system not supported yet."
-		}
-	}
+    default: {
+      fail "Operating system not supported yet."
+    }
+  }
 }
